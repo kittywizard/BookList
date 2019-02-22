48d8a74df9e224151f077991b89e0876e875098a
marleigh <volcana@gmail.com>
2019-02-21 22:18:11 -0800

    Very basic UI complete, starting JS

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("test");
+//event listener
+
+document.getElementById('input').addEventListener('submit', function(e){
+    grabData();
+
+    e.preventDefault();
+
+});
+
+
+function grabData(){
+    //grab all variables
+    const title = document.getElementById('bookTitle').value;
+    const author = document.getElementById('bookAuthor').value;
+    const position = parseInt(document.getElementById('listPosition').value);
+    //find a way to allow input of multiple tags
+    const tags = document.getElementById('tags').value;
+
+    //get UI 
+    const results = document.getElementById('results');
+    const card = document.querySelector('.card');
+    
+    //display everything in an unordered list
+    const createList = document.createElement('ul');
+    results.insertBefore(createList, card);
+
+    const listItem = document.createElement('li');
+    createList.insertBefore(listItem, );
+    
+}
